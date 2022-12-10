a5fb06545deb2551e40e7e9623a365e9de5350d2
Aniket <aniketkalbande16@gmail.com>
2022-12-10 06:37:52 +0530

    Added middleware

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 const express = require('express')
 const app = express();
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io')
 const ACTIONS = require('./src/Actions');
 
 const server = http.createServer(app);
 const io = new Server(server);
 const userSocketMap = {};
+
+app.use(express.static('build'));
+app.use((req, res ,next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 function getAllConnectedClients(roomId) {
    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
